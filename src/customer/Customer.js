2eb46b6bac7ebd2afdb7112474559f00069de5f5
daosuong <90263761+daosuong@users.noreply.github.com>
2021-11-06 20:34:53 +0700

    Add files via upload

--- src/customer/Customer.js
+++ src/customer/Customer.js
@@ -1,28 +1,60 @@
-import {Foods} from './Foods';
-import {Drinks} from './Drinks';
+import {Home} from './Home';
 import Cart from './Cart';
-import ReserveRoute from './component/router';
+import {Reservation} from './Reservation';
 import {BrowserRouter, Route, Switch, NavLink, Redirect} from 'react-router-dom';
 import Footer from './Footer';
-import React from 'react';
+import { MasterForm } from './Payment';
 
 const Customer = () => {
   return (
     <BrowserRouter>
-      <div className="App container">
+        <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+          <Route path = "/home" render={props =>
+            <div>
+              <DefaultContainer />
+              <Home />
+              <hr/>
+              <Footer/>
+            </div>
+          } />
+          <Route path = "/cart" render={props =>
+            <div>
+              <DefaultContainer />
+              <Cart />
+              <hr/>
+              <Footer/>
+            </div>
+          } />
+          <Route path = "/reservation" render={props =>
+            <div>
+              <DefaultContainer />
+              <Reservation />
+              <hr/>
+              <Footer/>
+            </div>
+          } />
+          <Route path = "/payment" component = {MasterForm}/>
+        </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Customer;
+
+function DefaultContainer() {
+  return (
+  <div className="default-container">
         <h3 className = "d-flex justify-content-center m-3" href = "#">
           Restaurant Name
         </h3>
         <nav className = "navbar navbar-expand-sm bg-light navbar-dark">
           <ul className = "navbar-nav">
             <li className = "nav-item- m-1">
-              <NavLink className = "btn btn-light btn-outline-primary" to = "/foods">
-                Foods
-              </NavLink>
-            </li>
-            <li className = "nav-item- m-1">
-              <NavLink className = "btn btn-light btn-outline-primary" to = "/drinks">
-                Drinks
+              <NavLink className = "btn btn-light btn-outline-primary" to = "/home">
+                Home
               </NavLink>
             </li>
             <li className = "nav-item- m-1">
@@ -36,25 +68,14 @@ const Customer = () => {
               </NavLink>
             </li>
             <li className="nav-item- m-1">
-              <a className="btn btn-light btn-outline-primary" href="/manage/login">
+              <a className="btn btn-light btn-outline-primary" href="/manage">
                 Quản lí
               </a>
             </li>
           </ul>
         </nav>
 
-        <Switch>
-          <Route path = "/foods" component = {Foods}/>
-          <Route path = "/drinks" component = {Drinks}/>
-          <Route path = "/cart" component = {Cart}/>
-          <Route path = "/reservation" component = {ReserveRoute}/>
-          <Redirect from="/" to="/home" />
-        </Switch>
-        <hr/>
-        <Footer/>
       </div>
-    </BrowserRouter>
   );
 }
-
-export default Customer;
+            
