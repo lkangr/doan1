a926288632a819ddac0585a1507bba8e307f8c6d
daosuong0112 <daosuong0112@gmail.com>
2022-12-07 01:49:54 +0700

    staff api and request from frontend done

--- frontend/src/manager/pages/emp_manager/Tables.js
+++ frontend/src/manager/pages/emp_manager/Tables.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 import React from 'react';
 import axios from 'axios';
 
-const Tables = ({begin}) => {
-  const [data, setData] = useState([]);
 
+const Tables = ({pos}) => {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`/api/staff?begin=${begin}`)
+      .get(`/api/staff?type=${pos}`)
       .then((res) => setData(res.data));
-  }, [begin])
+  }, [pos])
 
   return (
     <>
@@ -20,7 +20,7 @@ const Tables = ({begin}) => {
           <td><img src={"http://127.0.0.1:8000" + row.avatar} width={100} height={100} alt="Không có ảnh"/></td>
           <td>{row.name}</td>
           <td>{row.email}</td>
-          {row.status === 'S' ? <td>Shipper</td> : row.status === 'R' ? <td>Tiếp tân</td> : <td>Quản lý</td>}
+          {row.staff_type === 'S' ? <td>Shipper</td> : row.staff_type === 'R' ? <td>Tiếp tân</td> : <td>Quản lý</td>}
         </tr>)}
     </>
   )
